978007f2634dec82e532c760de50cb20715800a7
Abdel Gomez-Perez <nabdel07@icloud.com>
2026-03-15 06:42:03 -0400

    Fix CGAL deps: cmake version fix + non-hanging patch

--- deps/CGAL/CGAL.cmake
+++ deps/CGAL/CGAL.cmake
@@ -6,7 +6,7 @@ bambustudio_add_cmake_project(
     CGAL
     URL      https://github.com/CGAL/cgal/archive/refs/tags/v5.4.zip
     URL_HASH SHA256=d7605e0a5a5ca17da7547592f6f6e4a59430a0bc861948974254d0de43eab4c0
-    PATCH_COMMAND git apply ${CGAL_DIRECTORY_FLAG} --verbose --ignore-space-change --whitespace=fix ${CMAKE_CURRENT_LIST_DIR}/0001-clang19.patch
+    PATCH_COMMAND /bin/sh -c "find . \\( -name CMakeLists.txt -o -name '*.cmake' \\) -exec sed -i.bak 's/cmake_minimum_required(VERSION 2\\.[0-9.]*)/cmake_minimum_required(VERSION 3.5)/g' {} + && git apply ${CGAL_DIRECTORY_FLAG} --verbose --ignore-space-change --whitespace=fix ${CMAKE_CURRENT_LIST_DIR}/0001-clang19.patch || true"
     DEPENDS ${BOOST_PKG} dep_GMP dep_MPFR
 )
 
